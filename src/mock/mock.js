a921bcbefcf02c529eb7e076dce789dac9c4e6d2
Sender60 <butkov60@gmail.com>
2023-01-02 22:23:27 +0300

    Вывод сообщения на случай, если точек 0

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,6 +1,5 @@
 import {getRandomNumber, getRandomArrayElement} from '../util.js';
-const MIN_NUMBER = 1;
-const MAX_NUMBER = 10;
+import { MIN_NUMBER, MAX_NUMBER } from '../const.js';
 
 const offersTypes = [
   {
