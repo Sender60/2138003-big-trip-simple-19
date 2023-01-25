4e410e486e7461cd88e37c50d91688e7ff8d5ee4
Sender60 <butkov60@gmail.com>
2023-01-25 12:25:15 +0300

    Шаблонизируй то

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import TripPresenter from './presenter/trips-presenter.js';
 import PointsModel from './model/model.js';
 import {render} from './framework/render.js';
 import ListFilterView from './view/list-filter-view.js';
-import { generateFilter } from './mock/filter.js';
+import { getFilter } from './mock/filter.js';
 
 const tripEventsElement = document.querySelector('.trip-events');
 const headerElement = document.querySelector('.trip-controls');
@@ -13,7 +13,7 @@ const tripPresenter = new TripPresenter({
   pointsModel
 });
 
-const filters = generateFilter(pointsModel.points);
+const filters = getFilter(pointsModel.points);
 render(new ListFilterView({filters}), headerElement);
 
 tripPresenter.init();
